04e043aad9ba2e7e2d61b93c22034faa57cf39f3
zephyr0sjs <zephyr0s.js@gmail.com>
2022-01-09 19:18:22 +0900

    lec 6.2 - useEffect Deps

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
 
-  // state가 변할때마다 호출되지 않고 한번만 실행시키고 싶은 함수는 useEffect 사용
+  // state가 변할때마다 호출되지 않고 한번만 실행. []가 빈칸이기 때문
   useEffect(() => {
-    console.log("only onece");
+    console.log("I run only onece");
   }, []);
+  // keyword 가 변할때에만 실행됨
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
 
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      ></input>
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
